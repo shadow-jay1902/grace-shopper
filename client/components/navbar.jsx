f6b3f0ee18d65abac897d39df647820c0897c4cd
Omar Juice <ojameer1@gmail.com>
2019-04-16 15:41:42 -0400

    bulma, styled-jsx, functional navbvar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,31 +1,75 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logout } from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
+const Navbar = ({ handleClick, isLoggedIn }) => (
+  // <div>
+  //   <h1>BOILERMAKER</h1>
+  //   <nav>
+  //     {isLoggedIn ? (
+  //       <div>
+  //         {/* The navbar will show these links after you log in */}
+  //         <Link to="/home">Home</Link>
+  //         <a href="#" onClick={handleClick}>
+  //           Logout
+  //         </a>
+  //       </div>
+  //     ) : (
+  //       <div>
+  //         {/* The navbar will show these links before you log in */}
+  //         <Link to="/login">Login</Link>
+  //         <Link to="/signup">Sign Up</Link>
+  //       </div>
+  //     )}
+  //   </nav>
+  //   <hr />
+  // </div>
+  <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="https://bulma.io">
+        <img src="https://images.emojiterra.com/google/android-nougat/512px/1f6d2.png" />
+      </a>
+
+      <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div className="navbar-menu">
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            {isLoggedIn ?
+              <a onClick={handleClick} className="button is-warning">
+                <strong>Logout</strong>
+              </a> : <>
+                <Link to="/login">
+                  <a className="button is-primary">
+                    <strong>Login</strong>
+                  </a>
+                </Link>
+                <Link to="/signup">
+                  <a className="button is-light">
+                    Sign Up
           </a>
+                </Link>
+              </>}
+          </div>
         </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+      </div>
+    </div>
+    <style jsx>{`
+      .navbar-item img{
+        transform: scale(2,2);
+        position: relative;
+        left: 1rem;
+      }
+      `}</style>
+  </nav>
 )
 
 /**
