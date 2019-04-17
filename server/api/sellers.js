49231ef54e04dd084015694d7ff36dc8dbe62c44
Kenny Ye <kye6932@gmail.com>
2019-04-17 17:03:46 -0400

    Seller Model and Routes

---
+++ server/api/sellers.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const Seller = require('../db/models')
+
+router.get('/', async (req, res, next) => {
+  try {
+    console.log(Seller)
+    const sellers = await Seller.findAll()
+    res.json(sellers)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+    const seller = await Seller.findByPk(id)
+    res.json(seller)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async ({ body }, res, next) => {
+  try {
+    const seller = await Seller.create(body)
+    res.json(seller)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
