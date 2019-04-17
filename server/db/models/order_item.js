6b233551702b1cb1c7cda878cf50a92498fdfa65
Kenny Ye <kye6932@gmail.com>
2019-04-17 17:45:16 -0400

    Cleaning up code

--- server/db/models/order_item.js
+++ server/db/models/order_item.js
@@ -1,26 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-// db.query(`CREATE TABLE order_items
-// (
-//          userId INTEGER NOT NULL,
-//          itemId INTEGER NOT NULL,
-//          tag1 VARCHAR(20),
-//          tag2 VARCHAR(20),
-//          tag3 VARCHAR(20),
-//          PRIMARY KEY(question_id, tag_id)
-// );`)
 
 const OrderItem = db.define('order_item', {
-    // orderId: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     primaryKey: true
-    // },
-    // itemId: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     primaryKey: true
-    // },
     quantity: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -28,4 +9,5 @@ const OrderItem = db.define('order_item', {
     },
 
 })
+
 module.exports = OrderItem
