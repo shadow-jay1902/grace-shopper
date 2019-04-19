4667b1b028bb21e17106a2b0465d09bde165e7c3
Logan Takahashi <logan.takahashi@gmail.com>
2019-04-19 16:04:29 -0400

    uncentered all-items button

--- script/seed.js
+++ script/seed.js
@@ -1,37 +1,41 @@
 'use strict'
 
 const db = require('../server/db')
-const { User, Item } = require('../server/db/models')
-const faker = require('faker');
+const {User, Item} = require('../server/db/models')
+const faker = require('faker')
 
 async function seed() {
-  await db.sync({ force: true })
+  await db.sync({force: true})
   console.log('db synced!')
 
-  const users = Array(20).fill('x').map(() => ({
-    email: faker.internet.email(),
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    password: faker.internet.password(),
-    address: faker.address.streetAddress(),
-    phone: faker.phone.phoneNumber(),
-    dob: faker.date.past()
-  }))
+  const users = Array(20)
+    .fill('x')
+    .map(() => ({
+      email: faker.internet.email(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      password: faker.internet.password(),
+      address: faker.address.streetAddress(),
+      phone: faker.phone.phoneNumber(),
+      dob: faker.date.past()
+    }))
 
-  await User.bulkCreate(users);
+  await User.bulkCreate(allusers)
 
-  const categories = ['sports', 'food', 'clothes', 'collectables', 'hype'];
+  const categories = ['sports', 'food', 'clothes', 'collectables', 'hype']
 
-  const items = Array(200).fill('x').map(() => ({
-    name: faker.commerce.productName(),
-    description: faker.lorem.sentence(),
-    price: faker.commerce.price() / 100,
-    stock: Math.floor(Math.random() * 100),
-    category: categories[Math.floor(Math.random() * categories.length)],
-    photoURLs: [faker.image.image()]
-  }))
+  const items = Array(200)
+    .fill('x')
+    .map(() => ({
+      name: faker.commerce.productName(),
+      description: faker.lorem.sentence(),
+      price: faker.commerce.price() / 100,
+      stock: Math.floor(Math.random() * 100),
+      category: categories[Math.floor(Math.random() * categories.length)],
+      photoURLs: [faker.image.image()]
+    }))
 
-  await Item.bulkCreate(items);
+  await Item.bulkCreate(items)
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
