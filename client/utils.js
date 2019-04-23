a532b7c4807cd9c14d911a88fbc0e7d75556c17c
Logan Takahashi <logan.takahashi@gmail.com>
2019-04-23 12:06:16 -0400

    fixed price decimal bug in cart

--- client/utils.js
+++ client/utils.js
@@ -1,10 +1,10 @@
-export const decimalCleaner = (num) => {
-    console.log(num);
-    const splitVal = String(num).split('.')
-    while (splitVal[1] && splitVal[1].length < 2) splitVal[1] += '0'
-    if (!splitVal[1]) {
-        splitVal.push('00')
-    }
-    return splitVal[0] + '.' + splitVal[1].slice(0, 2)
-    return num
+export const decimalCleaner = num => {
+  console.log(num)
+  const splitVal = String(num / 100).split('.')
+  while (splitVal[1] && splitVal[1].length < 2) splitVal[1] += '0'
+  if (!splitVal[1]) {
+    splitVal.push('00')
+  }
+  return splitVal[0] + '.' + splitVal[1].slice(0, 2)
+  return num
 }
