defe3f6daddf45db8ac408c279ef8f6dd0fd29b9
Logan Takahashi <logan.takahashi@gmail.com>
2019-04-23 11:28:27 -0400

    guest checkout redirects to login

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {request} from 'https'
 
 const GOT_ITEMS_FROM_CART = 'GOT_ITEMS_FROM_CART'
 const SET_ITEM_ONTO_CART = 'SET_ITEM_ONTO_CART'
@@ -10,29 +11,41 @@ const initialState = {
   items: []
 }
 
-const gotItemsFromCart = cart => ({ type: GOT_ITEMS_FROM_CART, cart })
+const gotItemsFromCart = cart => ({type: GOT_ITEMS_FROM_CART, cart})
 
-const setItemOntoCart = item => ({ type: SET_ITEM_ONTO_CART, item })
+const setItemOntoCart = item => ({type: SET_ITEM_ONTO_CART, item})
 
-const removeFromCart = itemId => ({ type: REMOVED_FROM_CART, itemId })
+const removeFromCart = itemId => ({type: REMOVED_FROM_CART, itemId})
 
 const checkoutCart = () => ({
   type: CHECKOUT_CART
 })
 const editCart = (id, quantity) => ({
   type: EDIT_CART,
-  id, quantity
+  id,
+  quantity
 })
 
 export const getItemsFromCart = () => async (dispatch, getState) => {
   try {
     if (getState().user.id) {
-      const res = await axios.get('/api/cart')
+      let res = await axios.get('/api/cart')
+      let cart = window.localStorage.getItem('cart')
+      if (cart) {
+        cart = JSON.parse(cart)
+        const requests = []
+        for (let item of cart) {
+          requests.push(axios.post('/api/cart', item))
+        }
+        await Promise.all(requests)
+        window.localStorage.setItem('cart', '')
+        res = await axios.get('/api/cart')
+      }
       dispatch(gotItemsFromCart(res.data))
     } else {
       let cart = window.localStorage.getItem('cart')
       if (!cart) {
-        let newCart = { items: [] }
+        let newCart = {items: []}
         window.localStorage.setItem('cart', JSON.stringify(newCart))
         cart = newCart
       }
@@ -55,7 +68,10 @@ export const getItemOntoCart = item => async (dispatch, getState) => {
       const newCart = {
         ...currCart,
 
-        items: [...currCart.items.filter(({ id }) => id !== item.id), { ...item, quantity: 1 }]
+        items: [
+          ...currCart.items.filter(({id}) => id !== item.id),
+          {...item, quantity: 1}
+        ]
       }
       window.localStorage.setItem('cart', JSON.stringify(newCart))
       dispatch(setItemOntoCart(item))
@@ -86,16 +102,21 @@ export const removeFromCartThunk = itemId => async (dispatch, getState) => {
   }
 }
 
-export const editItemOnCart = (itemId, quantity) => async (dispatch, getState) => {
+export const editItemOnCart = (itemId, quantity) => async (
+  dispatch,
+  getState
+) => {
   try {
     if (getState().user.id) {
-      await axios.put(`/api/cart/`, { itemId, quantity })
+      await axios.put(`/api/cart/`, {itemId, quantity})
       dispatch(editCart(itemId, quantity))
     } else {
       const currCart = JSON.parse(window.localStorage.getItem('cart'))
       const newCart = {
         ...currCart,
-        items: currCart.items.map((item) => item.id === itemId ? { ...item, quantity } : item)
+        items: currCart.items.map(
+          item => (item.id === itemId ? {...item, quantity} : item)
+        )
       }
       window.localStorage.setItem('cart', JSON.stringify(newCart))
       dispatch(editCart(itemId, quantity))
@@ -104,22 +125,31 @@ export const editItemOnCart = (itemId, quantity) => async (dispatch, getState) =
     console.log(e)
   }
 }
-export const checkoutCartThunk = cb => async dispatch => {
+export const checkoutCartThunk = (cb, redirect) => async (
+  dispatch,
+  getState
+) => {
   try {
-    await axios.put('/api/cart/checkout')
-    dispatch(checkoutCart())
-    cb()
+    console.log('REDIRECT: ', redirect)
+    if (getState().user.id) {
+      await axios.put('/api/cart/checkout')
+      dispatch(checkoutCart())
+      cb()
+      window.localStorage.setItem('cart', '')
+    } else {
+      redirect()
+    }
   } catch (error) {
     console.log(error)
   }
 }
 
-export const cartReducer = function (state = initialState, action) {
+export const cartReducer = function(state = initialState, action) {
   switch (action.type) {
     case GOT_ITEMS_FROM_CART:
       return action.cart
     case SET_ITEM_ONTO_CART:
-      return { ...state, items: [...state.items, action.item] }
+      return {...state, items: [...state.items, action.item]}
     case REMOVED_FROM_CART:
       return {
         ...state,
@@ -131,7 +161,10 @@ export const cartReducer = function (state = initialState, action) {
       console.log(action.quantity)
       return {
         ...state,
-        items: state.items.map((item) => item.id === action.id ? { ...item, quantity: action.quantity } : item)
+        items: state.items.map(
+          item =>
+            item.id === action.id ? {...item, quantity: action.quantity} : item
+        )
       }
     default:
       return state
