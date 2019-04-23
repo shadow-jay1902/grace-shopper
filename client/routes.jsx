db3bc3117479b254134161e72c395890de70d790
Brandon Rowe <brandonhrowe@gmail.com>
2019-04-23 17:51:50 -0400

    home page for guest and user

--- client/routes.jsx
+++ client/routes.jsx
@@ -40,6 +40,7 @@ class Routes extends Component {
             {/* Routes placed here are only available after logging in */}
             <Route path="/order_history" component={ConnectedOrderHistory} />
             <Route path="/home" component={UserHome} />
+            <Route component={AllItems} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
